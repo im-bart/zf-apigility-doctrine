671f4aa0abdd599fb6e0264f62fcaec3d87fe5ad
Bart Brinkman <bart@instantmagazine.com>
2016-11-01 10:41:24 +0100

    added hack to deal with inner join and pagination, also cleaned up adapter itself

--- src/Server/Paginator/Adapter/DoctrineOrmAdapter.php
+++ src/Server/Paginator/Adapter/DoctrineOrmAdapter.php
@@ -21,15 +21,27 @@ class DoctrineOrmAdapter extends Paginator implements AdapterInterface
      */
     public function getItems($offset, $itemCountPerPage)
     {
-        if(array_key_exists($offset, $this->cache)
-                && array_key_exists($itemCountPerPage, $this->cache[$offset]) )
-
+        if (array_key_exists($offset, $this->cache)
+            && array_key_exists($itemCountPerPage, $this->cache[$offset])
+        ) {
             return $this->cache[$offset][$itemCountPerPage];
+        }
+
         $this->getQuery()->setFirstResult($offset);
         $this->getQuery()->setMaxResults($itemCountPerPage);
 
-        if(!array_key_exists($offset, $this->cache)) $this->cache[$offset] = array();
-        $this->cache[$offset][$itemCountPerPage] =  $this->getQuery()->getResult();
+        /**
+         * @see http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/dql-doctrine-query-language.html#first-and-max-result-items-dql-query-only
+         */
+        if (strstr($this->getQuery()->getDql(), 'INNER JOIN')) {
+            $this->getQuery()->setMaxResults(null);
+        }
+
+        if (! array_key_exists($offset, $this->cache)) {
+            $this->cache[$offset] = [];
+        }
+
+        $this->cache[$offset][$itemCountPerPage] = $this->getQuery()->getResult();
 
         return $this->cache[$offset][$itemCountPerPage];
     }
